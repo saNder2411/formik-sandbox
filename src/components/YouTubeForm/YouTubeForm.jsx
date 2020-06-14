69c81ef5e6336c56bba4f8755c41f1822ac6bc48
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-14 17:53:57 +0300

    Manually trigering validation

--- src/components/YouTubeForm/YouTubeForm.jsx
+++ src/components/YouTubeForm/YouTubeForm.jsx
@@ -48,135 +48,176 @@ const YouTubeForm = () => {
         validationSchema={validationSchema}
         onSubmit={onSubmit}
         validateOnChange={false}>
-        <Form>
-          <div className="form-control">
-            <label htmlFor="name">Name</label>
-            <Field
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"/>
+        {
+          ({validateForm, validateField, resetForm, setTouched, setFieldTouched}) => (
+            <Form>
+              <div className="form-control">
+                <label htmlFor="name">Name</label>
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"/>
 
-            <ErrorMessage name="name" component="div" className="error" />
-          </div>
+                <ErrorMessage name="name" component="div" className="error" />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="email">E-mail</label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"/>
+              <div className="form-control">
+                <label htmlFor="email">E-mail</label>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"/>
 
-            <ErrorMessage name="email" component={ErrorText} />
-          </div>
+                <ErrorMessage name="email" component={ErrorText} />
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="address">Address</label>
-            <FastField
-              name="address"
-              validate={validateAddress}>
-              {({field, meta}) => (
-                  <>
-                    <input
-                      type="text"
-                      id="address"
-                      placeholder="Your address"
-                      {...field}/>
-                      {meta.touched && meta.error ? (
-                        <div className="error">{meta.error}</div>
-                      ) : null}
-                  </>
-                )}
-            </FastField>
-          </div>
+              <div className="form-control">
+                <label htmlFor="address">Address</label>
+                <FastField
+                  name="address"
+                  validate={validateAddress}>
+                  {({field, meta}) => (
+                      <>
+                        <input
+                          type="text"
+                          id="address"
+                          placeholder="Your address"
+                          {...field}/>
+                          {meta.touched && meta.error ? (
+                            <div className="error">{meta.error}</div>
+                          ) : null}
+                      </>
+                    )}
+                </FastField>
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="channel">Channel</label>
-            <Field
-              type="text"
-              id="channel"
-              name="channel"
-              placeholder="Channel Name"/>
+              <div className="form-control">
+                <label htmlFor="channel">Channel</label>
+                <Field
+                  type="text"
+                  id="channel"
+                  name="channel"
+                  placeholder="Channel Name"/>
 
-            <ErrorMessage name="channel">
-              {(errorMsg) => (
-                <div className="error">{errorMsg}</div>
-              )}
-            </ErrorMessage>
-          </div>
+                <ErrorMessage name="channel">
+                  {(errorMsg) => (
+                    <div className="error">{errorMsg}</div>
+                  )}
+                </ErrorMessage>
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="facebook">Facebook profile</label>
-            <Field
-              type="text"
-              id="facebook"
-              name="social.facebook"
-              placeholder="Link on your profile in facebook"/>
-          </div>
+              <div className="form-control">
+                <label htmlFor="facebook">Facebook profile</label>
+                <Field
+                  type="text"
+                  id="facebook"
+                  name="social.facebook"
+                  placeholder="Link on your profile in facebook"/>
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="twitter">Twitter profile</label>
-            <Field
-              type="text"
-              id="twitter"
-              name="social.twitter"
-              placeholder="Link on your profile in twitter"/>
-          </div>
+              <div className="form-control">
+                <label htmlFor="twitter">Twitter profile</label>
+                <Field
+                  type="text"
+                  id="twitter"
+                  name="social.twitter"
+                  placeholder="Link on your profile in twitter"/>
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="primaryPh">Home phone number</label>
-            <Field
-              type="text"
-              id="primaryPh"
-              name="phoneNumbers[0]"
-              placeholder="Your phone number"/>
-          </div>
+              <div className="form-control">
+                <label htmlFor="primaryPh">Home phone number</label>
+                <Field
+                  type="text"
+                  id="primaryPh"
+                  name="phoneNumbers[0]"
+                  placeholder="Your phone number"/>
+              </div>
 
-          <div className="form-control">
-            <label htmlFor="secondaryPh">Mobile phone number</label>
-            <Field
-              type="text"
-              id="secondaryPh"
-              name="phoneNumbers[1]"
-              placeholder="Your phone number"/>
-          </div>
+              <div className="form-control">
+                <label htmlFor="secondaryPh">Mobile phone number</label>
+                <Field
+                  type="text"
+                  id="secondaryPh"
+                  name="phoneNumbers[1]"
+                  placeholder="Your phone number"/>
+              </div>
 
-          <div className="form-control">
-            <label>List of phone numbers</label>
-            <FieldArray name="phNumbers">
-              {
-                ({push, remove, form}) => (
-                  <div>
-                    {
-                      form.values.phNumbers.map((item, indx) => (
-                        <div key={Math.random() * 1e9}>
-                          <Field type="text" name={`phNumbers[${indx}]`} />
-                          <button type="button" onClick={() => push()}>+</button>
-                          {indx > 0 && <button type="button" onClick={() => remove()}>-</button>}
-                        </div>
-                      ))
-                    }
-                  </div>
-                )
-              }
-            </FieldArray>
-          </div>
+              <div className="form-control">
+                <label>List of phone numbers</label>
+                <FieldArray name="phNumbers">
+                  {
+                    ({push, remove, form}) => (
+                      <div>
+                        {
+                          form.values.phNumbers.map((item, indx) => (
+                            <div key={Math.random() * 1e9}>
+                              <Field type="text" name={`phNumbers[${indx}]`} />
+                              <button type="button" onClick={() => push()}>+</button>
+                              {indx > 0 && <button type="button" onClick={() => remove()}>-</button>}
+                            </div>
+                          ))
+                        }
+                      </div>
+                    )
+                  }
+                </FieldArray>
+              </div>
 
 
-          <div className="form-control">
-            <label htmlFor="comments">Comments</label>
-            <Field
-              as="textarea"
-              id="comments"
-              name="comments"
-              placeholder="Your comments"/>
+              <div className="form-control">
+                <label htmlFor="comments">Comments</label>
+                <Field
+                  as="textarea"
+                  id="comments"
+                  name="comments"
+                  placeholder="Your comments"/>
 
-            <ErrorMessage name="comments" component={ErrorText} />
-          </div>
-
-          <button type="submit">Submit</button>
-        </Form>
+                <ErrorMessage name="comments" component={ErrorText} />
+              </div>
+              
+              <button
+                type="button"
+                onClick={
+                  () => {
+                    setFieldTouched(`address`);
+                    validateField(`address`);
+                  }
+                }>
+                Validate Address
+              </button>
+              <button
+                type="button"
+                onClick={
+                  () => {
+                    setFieldTouched(`email`);
+                    validateField(`email`);
+                  }
+                }>
+                Validate Email
+              </button>
+              <button
+                type="button"
+                onClick={
+                  () => {
+                    setTouched({
+                      name: true,
+                      email: true,
+                      address: true,
+                      channel: true,
+                      comments: true,
+                    });
+                    validateForm();
+                  }
+                }>
+                Validate All
+              </button>
+              <button type="reset" onClick={() => resetForm()}>Reset All Field</button>
+              <button type="submit">Submit</button>
+            </Form>
+          )
+        }
       </Formik>
     </div>
   )
